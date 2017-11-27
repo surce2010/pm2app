eb027c5fec0f782d8b95b37b704c740c14fcae03
dingshaohua <345561171@qq.com>
2017-11-27 15:16:24 +0800

    bugfix

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-let http = require('http');
+let http = require("http");
 
-http.createServer(function(req, res) {
-	       res.end('pm2App')
-}).listen(8001);
+http
+	.createServer(function(req, res) {
+		res.end("pm2App");
+	})
+	.listen(8001);
 
-console.log('server running port 8001');
+console.log("server running port 8001");
